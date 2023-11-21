c67f10833fd84c3e7fccf0ce9a67e4680ee854f3
mahdi gh <optimus001kk@gmail.com>
2023-11-21 11:23:19 -0800

    modal handle and mini slider js added

---
+++ javaScript/handleModal.js
@@ -0,0 +1,8 @@
+exitModal.onclick = function (){
+
+    sModalOpen.style.display = 'none'
+}
+interstingSModal.onclick = function (){
+    sModalOpen.style.display = 'flex';
+    console.log('1');
+}
